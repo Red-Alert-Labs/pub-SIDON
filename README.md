88cb0b01a0dd83eff2954a54aec56ce9c89fcaf4
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2019-09-30 04:42:26 +0200

    Docker Compose to manage server app and database

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # SIDON
 
+Install the latest version of Docker and docker-compose
+
+RUN
+docker-compose build;
+docker-compose up -d database;
+docker-compose run --rm sidon-app /bin/bash -c "./manage.py migrate";
+docker-compose up -d;
+docker-compose run sidon-app ./manage.py collectstatic --no-input;
--- sidon-app/sidon/settings.py
+++ sidon-app/sidon/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sidon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sidon_db',
+        'USER': 'sidon_test_user',
+        'PASSWORD': 'sidon_test_password',
+        'HOST': 'database',  # <-- IMPORTANT: same name as docker-compose service!
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'djangoapp/static')
+MEDIA_ROOT = os.path.join(os.path.dirname(os.path.dirname(BASE_DIR)), 'djangoapp/media')
