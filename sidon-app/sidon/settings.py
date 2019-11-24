65fd3c1962a6590998cf39bbbbd4fdf9fd5b31e5
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2019-11-24 05:22:20 +0100

    Django settings updated for allowed hosts.

--- sidon-app/sidon/settings.py
+++ sidon-app/sidon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'eio&o8gs-*)on_la5wl8egcrsm^l&k81wp+8wrd%bdi8-%dykw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['15.188.48.8', 'localhost']
 
 
 # Application definition
