6f37fd59a41f434d24d814bccefbd6192bb22d3b
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2019-11-24 03:51:07 +0100

    AWS config file and auto restart scripts

--- scripts/restart-server.sh
+++ scripts/restart-server.sh
@@ -1,2 +1,5 @@
-#docker-compose -f /opt/app/docker-compose.yml stop server-app
-#docker-compose -f /opt/app/docker-compose.yml up -d --build server-app
+docker-compose -f /opt/app/docker-compose.yml build;
+docker-compose -f /opt/app/docker-compose.yml up -d database;
+docker-compose -f /opt/app/docker-compose.yml run --rm sidon-app /bin/bash -c "./manage.py migrate";
+docker-compose -f /opt/app/docker-compose.yml up -d;
+docker-compose -f /opt/app/docker-compose.yml run sidon-app ./manage.py collectstatic --no-input;
