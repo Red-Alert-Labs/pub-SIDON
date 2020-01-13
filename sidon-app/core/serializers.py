2ad2a31f3f84b514b54b1380de8e044212d20b40
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-01-13 02:30:47 +0100

    Serializer updated to handle nested objects

--- sidon-app/core/serializers.py
+++ sidon-app/core/serializers.py
@@ -2,15 +2,18 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from .models import RequirementsGroup, CommonCriteria
 
-class RequirementsGroupSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = RequirementsGroup
-        fields = ('id', 'name', 'requirements')
+
 
 class CommonCriteriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = CommonCriteria
         fields = ('id', 'name', 'cwe_id')
+
+class RequirementsGroupSerializer(serializers.ModelSerializer):
+    requirements = CommonCriteriaSerializer(read_only=True, many=True)
+    class Meta:
+        model = RequirementsGroup
+        fields = ('id', 'name', 'requirements')
 
 class UserSerializer(serializers.ModelSerializer):
     """docstring for UserSerializer."""
