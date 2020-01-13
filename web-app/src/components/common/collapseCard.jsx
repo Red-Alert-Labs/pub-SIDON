1e98ae2f58500120654759fddeb6794c45946420
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-01-13 03:19:58 +0100

    Works internally

--- web-app/src/components/common/collapseCard.jsx
+++ web-app/src/components/common/collapseCard.jsx
@@ -1,19 +1,26 @@
 import React, { Component } from "react";
 
 class CollapseCard extends Component {
-  state = {};
+  state = { expand: false };
+
+  handleToggleCard = () => {
+    const expand = !this.state.expand;
+    console.log("Click");
+    this.setState({ expand });
+  };
+
   render() {
     let classElement = "d-block card-header py-3";
     let aria = "false";
     let internalClass = "collapse";
-    if (!this.props.expand) {
+    if (!this.state.expand) {
       classElement += " collapsed";
     } else {
       aria = "true";
       internalClass += " show";
     }
     return (
-      <div className="card shadow mb-4">
+      <div className="card shadow mb-1">
         <a
           href="#collapseCardExample"
           className={classElement}
@@ -21,7 +28,7 @@ class CollapseCard extends Component {
           role="button"
           aria-expanded={aria}
           aria-controls="collapseCardExample"
-          onClick={this.props.onClick}
+          onClick={this.handleToggleCard}
         >
           <h6 className="m-0 font-weight-bold text-primary">
             {this.props.title}
