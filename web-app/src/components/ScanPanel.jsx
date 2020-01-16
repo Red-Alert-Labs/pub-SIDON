22dfc508c8748ed7d6d2f41589c7176b1d5ddf14
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-01-16 04:19:33 +0100

    Requirement selection UI buttons

--- web-app/src/components/ScanPanel.jsx
+++ web-app/src/components/ScanPanel.jsx
@@ -8,13 +8,23 @@ class ScanPanel extends Component {
         <div className="wrapper">
           <h3 className="inline">Select the Binary/Object file</h3>
         </div>
-
+        <hr />
         <div className="wrapper">
           <div className="file-upload">
             <input type="file" />
             <i className="fa fa-arrow-up"></i>
           </div>
         </div>
+        <div className="wrapper">
+          <p>No file selected</p>
+        </div>
+        <hr />
+        <div className="wrapper">
+          <h3>Select requirements</h3>
+          <button className="btn btn-secondary ml-2">From List</button>
+          <button className="btn btn-secondary ml-2">From File</button>
+        </div>
+        <hr />
         <div className="wrapper">
           <button className="btn btn-primary pl-4 pr-4">Scan</button>
         </div>
