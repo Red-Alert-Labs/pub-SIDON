bb9e9b2f8e54c43b406f74b62241fac7789488af
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-02-10 15:56:28 +0100

    removed fake data

--- web-app/src/components/ScanResults.jsx
+++ web-app/src/components/ScanResults.jsx
@@ -3,14 +3,6 @@ import Card from "./common/Card";
 import ResultsTable from "./resultsTable";
 import { getScans } from "../services/scansService";
 
-const fakeData = [
-  { id: 1, name: "DoorLock.bin", date: "12/12/2021" },
-  { id: 2, name: "SevenSensor", date: "09/12/2021" },
-  { id: 3, name: "DoorLock.bin", date: "12/12/2021" },
-  { id: 4, name: "Heater.bin", date: "19/08/2020" },
-  { id: 5, name: "DarkLight.bin", date: "01/08/2021" }
-];
-
 class ScanResults extends Component {
   state = {
     scans: []
