a4db1fb0b26bb9eb4234ffa40b61e49deb487663
Behan Remoshan BENET RUBAN <b.remoshan@gmail.com>
2020-02-14 07:54:06 +0100

    Scans with nav

--- web-app/src/components/resultsTable.jsx
+++ web-app/src/components/resultsTable.jsx
@@ -1,12 +1,17 @@
 import React, { Component } from "react";
 import Table from "./common/table";
+import { Link } from "react-router-dom";
 
 class ResultsTable extends Component {
   state = {};
 
   columns = [
     { path: "id", label: "ID" },
-    { path: "name", label: "Name" },
+    {
+      path: "name",
+      label: "Name",
+      content: scan => <Link to={`/scans/${scan.id}`}>{scan.name}</Link>
+    },
     { path: "uploaded_at", label: "Date" }
   ];
   render() {
